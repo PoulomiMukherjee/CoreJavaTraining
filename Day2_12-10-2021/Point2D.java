7f304652f018db8cb0bd45616a2476e2bf74bddc
PoulomiMukherjee <64145379+PoulomiMukherjee@users.noreply.github.com>
2021-10-12 11:57:38 +0530

    Create Point2D.java

---
+++ Day2_12-10-2021/Point2D.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class Point2D {
+	double x,y;
+	public Point2D(double x_val, double y_val){
+		x = x_val;
+		y = y_val;
+	}
+  
+	public String getDetails(){
+		return "Co-ordinates of p1: ("+x+", "+y+")";
+	}
+	
+}
